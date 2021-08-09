4ef1bd60d17700876063416fedf91955d5bb9699
Adriaan Veney <45220404+aveney@users.noreply.github.com>
2021-08-09 11:21:56 -0400

    Added current progress, updated UI, upload image on backend, and display image on frontend.

--- src/main/java/com/codedifferently/group2/Spyglass/SpyglassApplication.java
+++ src/main/java/com/codedifferently/group2/Spyglass/SpyglassApplication.java
@@ -1,9 +1,14 @@
 package com.codedifferently.group2.Spyglass;
 
+import com.codedifferently.group2.Spyglass.Property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class SpyglassApplication {
 
 	public static void main(String[] args) {
